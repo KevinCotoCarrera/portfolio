a9eff743363ed2c1099235acbe9bc43f1a4e3de4
Kevin Coto <kevincoto2002@gmail.com>
2021-07-06 23:58:49 -0400

    Header

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+
+<ul class="w-auto flex justify-around mt-2 lg:w-96 lg:absolute lg:top-4 lg:right-0">
+  <li><a href="#works" class="hover:text-pink-300 hover:underline text-lg lg:text-xl 2xl:text-2xl">Works</a></li>
+  <li><a href="https://blog.kevincoto.com" class="hover:text-pink-300 hover:underline text-lg lg:text-xl 2xl:text-2xl">Blog</a></li>
+  <li><a href="#contact" class="hover:text-pink-300 hover:underline text-lg lg:text-xl 2xl:text-2xl">Contact me</a></li>
+</ul>
+<div class="w-full flex flex-col items-center mt-16 lg:mt-28 2xl:mt-36">
+  <h2 class="text-7xl mr-40 lg:mr-96 lg:text-8xl 2xl:text-9xl">Hello.</h2>
+  <h2  class="text-7xl mr-14 lg:mr-48 mt-6 lg:text-8xl 2xl:text-9xl">I'm Kevin</h2>
+  <div class="flex mt-4 lg:mt-8 lg:mr-48 hover">
+    <span class="text-2xl lg:text-3xl">Frontend Developer</span>
+    <svg class="w-5 h-5 mt-2 ml-2 lg:w-6 lg:h-6" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.25 20H2.49999C1.11928 20 0 18.8808 0 17.5V3.75C0 2.36929 1.11928 1.25 2.49999 1.25H7.50001V3.75H2.49999V17.5H16.25V12.5H18.75V17.5C18.75 18.8808 17.6306 20 16.25 20ZM9.625 12.1337L7.8625 10.3663L15.7287 2.5H11.25V0H20V8.75H17.5V4.26875L9.625 12.1337Z" fill="#F9FAFF"/></svg>
+  </div>
+
+ <div class="flex flex-colum items-center hover:bg-pink-300 hover:border-opacity-0 hover:shadow hover:text-gray-900 hover:underline place-content-center w-48 h-48  rounded-full border-solid border-2 border-gray-500 mt-10 lg:absolute lg:right-40 lg:top-20 2xl:w-56 2xl:h-56 2xl:top-32"><span class="text-2xl 2xl:text-3xl">See my work</span></div>
+<div class="sm:w-96 w-64">
+<p class=" mt-16 text-lg text-gray-300 2xl:text-xl">I’m developer that create modern websites, resolving problems and I use necesary tecnologies for each projects, I’m in love with Web Design.</p>
+
+<p class="mt-4 text-lg text-gray-300 2xl:text-xl"><span class="text-pink-300 text-xl hover:underline -ml-8 sm:-ml-20 2xl:text-2xl">Contact </span> <span class="text-pink-300">>>></span> as a freelancer I will make a budget for each project and give you excellent results.</p>
+</div>
+<h2 id="works" class="mt-24 lg:mt-40 text-4xl lg:text-5xl">Works<span class="text-pink-300">___</span></h2>
+<h3 class="mt-16 lg:mt-32 text-3xl lg:text-4xl">I'm building this section.</h3>
+<h2  class="mt-24 lg:mt-40 text-4xl lg:text-5xl">Contact me<span class="text-pink-300">___</span></h2>
+<h3 id="contact" class="mt-16 lg:mt-32 text-3xl lg:text-4xl">I'm building this section.</h3>
+
+</div>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+#app{
+  color:#FFFF;
+  background-color: #282E36;
+}
+
+</style>
